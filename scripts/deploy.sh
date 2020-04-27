f2d3a74131dd7115beb05d2d6f9c3dad74c62646
Drew Buckman <drew.buckman@gmail.com>
2020-04-27 19:07:39 -0400

    testing an update agian

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,7 +2,7 @@
 
 echo "$KUBERNETES_CLUSTER_CERTIFICATE" | base64 --decode > cert.crt
 
-./kubectl \
+kubectl \
   --kubeconfig=/dev/null \
   --server=$KUBERNETES_SERVER \
   --certificate-authority=cert.crt \
