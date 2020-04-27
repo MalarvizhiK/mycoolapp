bdb234473a91a130f9538a512dac23fb2cb0a6cb
Drew Buckman <drew.buckman@gmail.com>
2020-04-27 11:36:44 -0400

    initial commit

---
+++ src/server.py
@@ -0,0 +1,14 @@
+import os
+from flask import Flask
+from datetime import datetime
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    today = datetime.now()
+    return "Hi. {}".format(today.strftime("%Y-%m-%d %H:%M:%S"))
+
+if __name__ == "__main__":
+    SERVER_PORT = os.environ.get("SERVER_PORT", "8000")
+    app.run(host="", port=int(SERVER_PORT))
