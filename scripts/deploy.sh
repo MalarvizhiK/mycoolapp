40c4d3d76ec6c49eef3e218a3f3f0c55f8e9b140
Drew Buckman <drew.buckman@gmail.com>
2020-04-27 17:39:33 -0400

    test travis 01

---
+++ scripts/deploy.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+set -e
+
+echo "$KUBERNETES_CLUSTER_CERTIFICATE" | base64 --decode > cert.crt
+
+./kubectl \
+  --kubeconfig=/dev/null \
+  --server=$KUBERNETES_SERVER \
+  --certificate-authority=cert.crt \
+  --token=$KUBERNETES_TOKEN \
+  apply -f ./kube/
