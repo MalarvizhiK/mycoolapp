380817d20caa4ee398f8623a14f769e2066f9319
Drew Buckman <drew.buckman@gmail.com>
2020-04-28 11:20:39 -0400

    try this

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -14,4 +14,4 @@ kubectl \
   --server=$KUBERNETES_SERVER \
   --certificate-authority=cert.crt \
   --token=$KUBERNETES_TOKEN \
-  set image deployment/pypytest pypytest=onestoop/pypytest:${TRAVIS_BUILD_NUMBER} --record
+  set image deployment/pypytest pypytest=onestoop/pypytest:$TRAVIS_BUILD_NUMBER --record
