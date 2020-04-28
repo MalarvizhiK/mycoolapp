5a8187995afa3547e9bd9391670f1f27c50d73d8
Drew Buckman <drew.buckman@gmail.com>
2020-04-28 10:18:19 -0400

    another take on another take

--- scripts/updateKube.sh
+++ scripts/updateKube.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-sed 's/VERSION/$TRAVIS_BUILD_NUMBER/g' ./kube/deployment.yml > .kube/deployment.yml
-cat .kube/deployment.yml
+sed 's/VERSION/$TRAVIS_BUILD_NUMBER/g' /kube/deployment.yml > kube/deployment.yml
+cat kube/deployment.yml
